172edf1639cfa2dfc7a374e8362dadcfc787afed
杨煜溟 <yangym@zju.edu.cn>
2018-07-12 04:54:28 +0800

    file 100

--- src/main/java/io/openmessaging/config/MessageStoreConfig.java
+++ src/main/java/io/openmessaging/config/MessageStoreConfig.java
@@ -39,7 +39,7 @@ public class MessageStoreConfig {
     private int fileSizeCommitLog = Integer.MAX_VALUE;
 
     /** 存储消息的文件数量 **/
-    public static final int numCommitLog = 55;
+    public static final int numCommitLog = 100;
 
     public int getFileSizeCommitLog() {
         return fileSizeCommitLog;
